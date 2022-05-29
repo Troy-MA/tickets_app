17405eb03cf80bcbee74d0e63466d574a20a830b
Troy-MA <57745873+Troy-MA@users.noreply.github.com>
2022-05-29 20:38:27 +0200

    init

--- lib/ui/ui_methods.dart
+++ lib/ui/ui_methods.dart
@@ -113,3 +113,121 @@ Padding textField() {
     ),
   );
 }
+
+SizedBox ticketCard({
+  required String imageUrl,
+  required String price,
+  required String description,
+  required String location,
+  required String mainAct,
+  required String name,
+}) {
+  return SizedBox(
+    width: 200,
+    height: 800,
+    child: Card(
+      elevation: 5,
+      color: Colors.grey[900],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          orangeDivider(),
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '\$$price',
+                          style: const TextStyle(
+                              fontSize: 25, color: Colors.orange),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Icon(
+                          Icons.location_on_outlined,
+                          color: Colors.orange,
+                        ),
+                        Text(
+                          location,
+                          style: TextStyle(color: Colors.grey[400]),
+                        )
+                      ],
+                    ),
+                    Text(
+                      description,
+                      textAlign: TextAlign.justify,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Main ',
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        const Icon(Icons.people_alt_outlined,
+                            color: Colors.orange),
+                        Text('' + mainAct)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            width: 200,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(12),
+                  bottomRight: Radius.circular(12)),
+              child: InkWell(
+                child: Container(
+                  color: Colors.orange,
+                  child: const Center(child: Text('Open')),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Container orangeDivider() {
+  return Container(
+    color: Colors.orange,
+    width: 200,
+    height: 2,
+  );
+}
